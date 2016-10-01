f16b47add68bfed9b36b11314c1c7dda85d7d165
Louis Henri Franc <louis.franc@insa-rouen.fr>
2016-10-01 13:09:47 -0400

    made some test

--- firstTry.cpp
+++ firstTry.cpp
@@ -3,6 +3,8 @@
 #include <cmath>
 #include <iostream>
 #include <cassert>
+#include <ctime>
+#include <chrono>
 
 using namespace std;
 // #include "tbb/tbb.h"
@@ -13,69 +15,152 @@ const auto binary_file_name = "binaryMatrix";
 
 
 void generateMatrixBinary() {
-	ofstream out(binary_file_name);
-	for(int i(0);i<binary_matrix_nb_row;i++) {
-		for(int j(0);j<binary_matrix_nb_col;j+=3) {
-			int k = rand() % 3;
-			out << (k == 0 ? 1 : 0) << "," << (k == 1 ? 1 : 0) << "," << (k == 2 ? 1 : 0) << ",";
-		}	
-		out << endl;
-	}
-	out.close();
+        ofstream out(binary_file_name);
+        for (int i(0); i < binary_matrix_nb_row; i++) {
+                for (int j(0); j < binary_matrix_nb_col; j += 3) {
+                        int k = rand() % 3;
+                        out << (k == 0 ? 1 : 0) << "," << (k == 1 ? 1 : 0) << "," << (k == 2 ? 1 : 0) << ",";
+                }
+                out << endl;
+        }
+        out.close();
 }
 
 
 class CompressMatrix {
-	CompressMatrix(const long int& nbRow,const long int& nbCol,const int& dispersion) :
-		m_dispersion(dispersion),
-		m_nbRow(nbCol) {
-		
-		if(dispersion == 1) {
-			nbBitForOneFeature = 1;
-		} else {
-			nbBitForOneFeature = ceil(log(dispersion) / log(2));
-		}
+public:
+        CompressMatrix(const long int& nbRow, const long int& nbCol, const int& dispersion) :
+                m_nbRow(nbRow),
+                m_oldnbCol(nbCol),
+                m_nbCol(ceil((double)(nbCol) / 32))
+        {
+                m = ((uint32_t **)malloc(m_nbRow * sizeof(uint32_t *)));
+        }
 
 
-		//assert(nbCol % dispersion == 0);
+        ~CompressMatrix() {
+                for (int32_t row(0); row < m_nbRow; row++) {
+                        // delete[] m[row];
+                }
+        }
+        void compressRow(const int& row, const int8_t* data, const int& N) {
+                assert(row < m_nbRow);
+                // delete[] m[row];
+                m[row] = (uint32_t  *)malloc(m_nbCol * sizeof(uint32_t *));
+                memset(m[row], 0, m_nbCol * sizeof(m[row]));
+                for (uint32_t i(0); i < N; i++) {
+                        int index = i / 32;
+                        int shift = i - index * 32;
+                        m[row][index] += (data[i] ? (1 << shift) : (0 << shift));
+                }
+        }
 
-		//m_nbCol = nbCol / dispersion;
-		//m_nbCol = ceil((double)(m_nbCol) / 32);
-		//m_nbCol *= nbBitForOneFeature;
-		m_nbCol = nbCol / 32;
+        void decompressRow(const int& row, int8_t* data) {
+                memset(data, 0, (m_oldnbCol) * sizeof(int8_t));
+                for (int i(0); i < m_oldnbCol; i++) {
+                        int index = i / 32;
+                        int8_t shift = i - index * 32;
+                        data[i] = ((m[row][index] & (1 << shift)) ? 1 : 0);
+                }
+        }
 
-		m_nbRow = nbRow;
+        inline float multiplyRow(const int& row, const float* rowA) {
+                float result = 0;
+                auto m_row = m[row];
+                for (int i(0); i < m_oldnbCol; i++) {
+                        int index = i / 32;
+                        int8_t shift = i - index * 32;
+                        int bit = m[row][index] & (1 << shift);
+                        float value = rowA[i];
+                        result += ((m[row][index] & (1 << shift)) ? rowA[i] : 0);
+                }
+                return result;
+        }
 
-		m = (int32_t **) malloc(m_nbRow * sizeof(int32_t*));
-	}
-
-
-	void compressRow(const int& row,const int8_t data[]) {
-		assert(row < m_nbRow);
-		delete[] m[row];
-		m[row] = (int32_t *) malloc(m_nbCol * sizeof(int32_t*));
-
-		unsigned int nbColData = sizeof(data) / sizeof(int8_t);
-		for(unsigned int32_t i(0);i<nbColData;i++) {
-			int index = m_nbCol / 32;
-			int shift = m_nbCol - shift * 32;
-			m[row][index] = (data[i] ? (1 << shift) : (0 << shift));
-		}
-	}
-
-	int8_t nbBitForOneFeature;
-	int8_t m_dispersion;
-	int32_t ** m;
-	int32_t m_nbRow;
-	int32_t m_nbCol;
+        uint32_t **m;
+        uint32_t  m_nbRow;
+        uint32_t  m_oldnbCol;
+        uint32_t  m_nbCol;
 };
 
-void compressMatrixBinary() {
-	//ifstream()
+void testCompression() {
+        CompressMatrix cp(100, 1000, 1);
+        int8_t data[1000];
+        for (int i(0); i < 1000; i++) {
+                data[i] = rand() % 2;
+        }
+
+
+        size_t N = sizeof(data) / sizeof(data[0]);
+
+        auto start = chrono::high_resolution_clock::now();
+        cp.compressRow(1, data, N);
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<double> diff = end - start;
+        cout << "Compressing row of size " << N << " :" << diff.count() << " s\n";
+
+        int8_t data_uncompress[1001];
+        start = chrono::high_resolution_clock::now();
+        cp.decompressRow(1, data_uncompress);
+        end = chrono::high_resolution_clock::now();
+        diff = end - start;
+        cout << "Decompressing row of size " << N << " :" << diff.count() << " s\n";
+        for (int i(0); i < 1000; i++) {
+                assert((data[i]) == (data_uncompress[i]));
+        }
+}
+
+
+
+void testMultiplyMatrix() {
+        const int sizeColomn = 1'000'000;
+        CompressMatrix cp(100, sizeColomn, 1);
+        int8_t data[sizeColomn];
+        for (int i(0); i < sizeColomn; i++) {
+                data[i] = rand() % 2;
+        }
+
+        size_t N = sizeof(data) / sizeof(data[0]);
+        cp.compressRow(1, data, N);
+
+
+        float* array;
+        array = (float *)malloc(sizeColomn * sizeof(float));
+
+        for (int i(0); i < sizeColomn; i++) {
+                array[i] = (float)rand() / (float)(RAND_MAX / 5.0);
+        }
+
+
+        float result = 0, result1 = 0;
+        auto start = chrono::high_resolution_clock::now();
+        for (int i(0); i < sizeColomn; i++) {
+                result += data[i] * array[i];
+        }
+        auto end = chrono::high_resolution_clock::now();
+        chrono::duration<double> diff = end - start;
+        cout << "Simple multiplication  :" << diff.count() << " s\n";
+
+
+        start = chrono::high_resolution_clock::now();
+        result1 = cp.multiplyRow(1, array);
+        end = chrono::high_resolution_clock::now();
+        diff = end - start;
+
+
+
+
+        cout << "Fancy row multiplication  :" << diff.count() << " s\n";
+
+        assert(result == result1);
 }
 
 int main()
-{	
-	int i[] = {1,2,3,4,5,6,9}	;
-	// generateMatrixBinary();
+{
+        srand(time(nullptr));
+        int i[] = { 1,2,3,4,5,6,9 };
+        // generateMatrixBinary();
+        testCompression();
+        testMultiplyMatrix();
 }
+
