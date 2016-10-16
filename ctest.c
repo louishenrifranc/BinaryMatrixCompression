4b05c2a5cb2d44983a4989ee9b8a5a141212a978
Louis Henri Franc <lofrab@l4714-24.info.polymtl.ca>
2016-10-16 17:41:54 -0400

    fonction C fonctionnel

--- ctest.c
+++ ctest.c
@@ -5,24 +5,37 @@
 
 void matmul(const int8_t* X, const double* W, const size_t nb_in, const size_t nb_out, const size_t nb_hidden, double* Y) {
     // Check if parallelism work
-   freopen("test.out","w",stdout);
-   {
-        int i;
-        #pragma omp parallel for private(i)
-        for(i = 0;i < nb_in*nb_hidden;i++){
-            printf("%d ",X[i]);
-        }
-        printf("\n");
-        #pragma omp parallel for private(i)
-        for(i = 0;i < nb_out*nb_hidden;i++){
-            printf("%lf ",W[i]);
-        }
-        printf("\n");
-        #pragma omp parallel for private(i)
-        for(i = 0;i < nb_in*nb_out;i++){
-            printf("%lf ",Y[i]);
-        }
-        printf("\n");
-   }
+   // freopen("test.out","w",stdout);
+   // {
+   //      int i;
+   //      #pragma omp parallel for private(i)
+   //      for(i = 0;i < nb_in*nb_hidden;i++){
+   //          printf("%d ",X[i]);
+   //      }
+   //      printf("\n");
+   //      #pragma omp parallel for private(i)
+   //      for(i = 0;i < nb_out*nb_hidden;i++){
+   //          printf("%lf ",W[i]);
+   //      }
+   //      printf("\n");
+   //      #pragma omp parallel for private(i)
+   //      for(i = 0;i < nb_in*nb_out;i++){
+   //          printf("%lf ",Y[i]);
+   //      }
+   //      printf("\n");
+   // }
 
+   // printf("%f %f",W[0], W[nb_hidden]);
+
+   for(size_t i=0;i<nb_in;i++) {
+        for(size_t k=0;k<nb_out;k++){
+            float res = 0;
+            for(int j=0;j<nb_hidden;j++) {
+                if(X[i*nb_hidden + j]) {
+                    res += W[k*nb_hidden + j];
+                }
+                Y[i*nb_out + k] = res;
+            }
+        }
+   }
 }
--- testFunction.py
+++ testFunction.py
@@ -12,23 +12,25 @@ intcpp = ndpointer(ctypes.c_int8, ndim=2, flags="C_CONTIGUOUS")  # ndpointer(dty
 matmul_c.argtypes = [intcpp, doublepp, ctypes.c_int, ctypes.c_int, ctypes.c_int, doublepp]
 
 np.random.seed(24)
-nb_col = 5
-nb_lig = 4
+nb_hidden = 5
+nb_in = 4
 nb_out = 3
 
-mat_binary = np.random.randint(2, size=(nb_lig, nb_col), dtype='int8')
-mat_float = np.random.uniform(-1, 1, size=(nb_out, nb_col))
+mat_binary = np.random.randint(2, size=(nb_in, nb_hidden), dtype='int8')
+mat_float = np.random.uniform(-1, 1, size=(nb_out, nb_hidden))
 
 print(mat_binary)
 print ''
 print(mat_float)
 print ''
 
-res = np.ones((nb_out, nb_lig), dtype='double')
+res = np.ones((nb_in, nb_out), dtype='double')
 
-matmul_c(mat_binary, mat_float, nb_lig, nb_out, nb_col, res)
-print(mat_binary)
-print ''
-print(mat_float)
+print(np.dot(mat_float, mat_binary.T))
+matmul_c(mat_binary, mat_float, nb_in, nb_out, nb_hidden, res)
+# print(mat_binary)
+# print ''
+# print(mat_float)
 print ''
 print(res)
+
